d6182a3d930f7929cd97e84dc65fdae0aa71f3a0
Cristian souto <souto.cristian@gmail.com>
2022-08-08 17:31:32 -0300

    incremento de los aciertos

--- js/app.js
+++ js/app.js
@@ -5,7 +5,10 @@ let card2 = null;
 let firstResult = null;
 let secondResult = null;
 let moves = 0;
+let hitIncrement = 0;
+
 let mostratMovimientos = document.getElementById("movimientos");
+let showHits = document.getElementById("hit");
 
 /*generamos los numeros aleatorios del array*/
 let numbers = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
@@ -35,7 +38,14 @@ function flip(id) {
         card2.disabled = true;
         //incrementamos la variable movimientos
         moves = moves + 1;
-        console.log(moves)
-        movimientos.innerHTML =`Movimientos ${moves}`;
+        mostratMovimientos.innerHTML =`Movimientos ${moves}`;
+        //las tarjetas son iguales
+        if(firstResult == secondResult){
+          cardFlip = 0;
+          //Acierto de tarjetas incremento los valores
+          hitIncrement++;
+          //mostramos los el valor en las estadicas de acierto
+          showHits.innerHTML = `Aciertos ${hitIncrement}`;
+        }
     }
 }
